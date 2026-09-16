b56e498d4aab754119ba4a858202d672849b0a68
Danilo <danilopmic@gmail.com>
2026-09-16 15:11:39 -0300

    feat: criando a função de salvar clientes no arquivo txt

--- sistema_cadastro.py
+++ sistema_cadastro.py
@@ -4,3 +4,7 @@ from colorama import Fore, Style, init  # importa recursos do colorama para adic
 init()  # inicia o colorama para permitir o uso das cores no terminal
 
 ARQUIVO = "clientes.txt"  # cria uma variável que armazena o nome do arquivo onde os clientes serão salvos
+
+def salvar_clientes_arquivo(nome, email, telefone):  # define a função responsável por salvar um cliente no arquivo
+    with open(ARQUIVO, "a", encoding="utf-8") as arquivo:  # abre o arquivo no modo append para adicionar os dados
+        arquivo.write(f"{nome};{email};{telefone}\n")  # escreve os dados do cliente no arquivo
