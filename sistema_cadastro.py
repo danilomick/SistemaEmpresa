696bab26d78f68c8111124221f3963cb5b652f97
Danilo <danilopmic@gmail.com>
2026-09-16 15:06:29 -0300

    feat: importa o sistema operacional e o colorama e cria o arquivo principal, iniciando a estrutura do sistema de clientes

---
+++ sistema_cadastro.py
@@ -0,0 +1,6 @@
+import os  # importa o módulo os, que permite trabalhar com arquivos e pastas do sistema
+from colorama import Fore, Style, init  # importa recursos do colorama para adicionar cores aos textos
+
+init()  # inicia o colorama para permitir o uso das cores no terminal
+
+ARQUIVO = "clientes.txt"  # cria uma variável que armazena o nome do arquivo onde os clientes serão salvos
