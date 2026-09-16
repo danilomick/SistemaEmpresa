a7d10f60b5e69507d3899a40b47f03444d970363
Rezende-Pedro <pe300809@gmail.com>
2026-09-16 15:11:45 -0300

    feat:Função de cadastro do valor

--- sistema_produto.py
+++ sistema_produto.py
@@ -14,3 +14,21 @@ def cadastro_produto():
             print(Fore.RED + "Erro! Digite somente letras.\n" + Style.RESET_ALL)
         else:
             return produto
+        
+def cadastro_valor(nome_produto):
+    while True:
+        preco = input(Fore.LIGHTWHITE_EX+f"Digite o valor de {nome_produto}: "+ Style.RESET_ALL).strip().replace(',', '.')
+        
+        if len(preco) == 0:
+            print(Fore.RED + "Erro! Este campo não pode ficar em branco.\n" + Style.RESET_ALL)
+            continue 
+            
+        try:
+            # preco é convertido em Float agora, porque não é aceito strip em entrada float
+            valor = float(preco)
+            if valor <= 0:
+                print(Fore.RED + "Erro! O valor deve ser maior que zero\n" + Style.RESET_ALL)
+                continue
+            return valor
+        except ValueError:
+            print(Fore.RED + "Erro! Valor inválido. Digite um número.\n" + Style.RESET_ALL)
